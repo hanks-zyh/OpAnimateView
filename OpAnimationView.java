a6703d32eb0fad929382ecf6ac5d463fb9a0ae5a
hanks <1161745215@qq.com>
2015-05-29 13:58:54 +0800

    add the method setShapeRadius(int radius)   to change shape size

--- OpAnimationView.java
+++ OpAnimationView.java
@@ -43,6 +43,8 @@ public class OpAnimationView extends View {
     private int shadowWidth;        //shadow randius
     private int defaultShapeWidth;
     private int shapeWidth;
+    private int shapeRadius;
+    private int defaultShapeRadius;
 
     public OpAnimationView(Context context) {
         this(context, null);
@@ -72,6 +74,7 @@ public class OpAnimationView extends View {
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
 
         defaultShapeWidth = dp2px(4);
+        defaultShapeRadius = dp2px(10);
 
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
@@ -88,7 +91,9 @@ public class OpAnimationView extends View {
         setBackgroundColor(attributes.getColor(R.styleable.OpAnimationView_backgroundColor, Color.parseColor("#FF4081")));
         setShapeColor(attributes.getColor(R.styleable.OpAnimationView_shapeColor, Color.parseColor("#ffffff")));
         shapeWidth = attributes.getDimensionPixelOffset(R.styleable.OpAnimationView_shapeWidth, defaultShapeWidth);
+        shapeRadius = attributes.getDimensionPixelOffset(R.styleable.OpAnimationView_shapeRadius, defaultShapeRadius);
         setShapeWidth(shapeWidth);
+        setShapeRadius(shapeRadius);
 
         // We no longer need our attributes TypedArray, give it back to cache
         attributes.recycle();
@@ -108,14 +113,30 @@ public class OpAnimationView extends View {
         c = (int) (r / g2);
         float dis = r - c + dp2px(4);
 
+        float x = w / 2;
+        float y = h / 2;
+
         points.clear();
-        points.add(new PointF(c + dis, c / 3f + dis));
+
+
+        float d = shapeWidth / g2 / 2;
+        points.add(new PointF(x, y - shapeRadius));
+        points.add(new PointF(x, y + shapeRadius));
+        points.add(new PointF(x - shapeRadius, y));
+        points.add(new PointF(x + shapeRadius, y));
+        points.add(new PointF(x - shapeRadius - shapeRadius, y));
+        points.add(new PointF(x - (2 - g2) * shapeRadius + d, y + g2 * shapeRadius + d));
+        points.add(new PointF(x - (2 - g2) * shapeRadius - d, y + g2 * shapeRadius + d));
+        points.add(new PointF(x + (3*g2-2) * shapeRadius, y - g2 * shapeRadius));
+
+
+        /*points.add(new PointF(c + dis, c / 3f + dis));
         points.add(new PointF(c + dis, c * 5f / 3f + dis));
         points.add(new PointF(c / 3f + dis, c + dis));
         points.add(new PointF(c * 5f / 3f + dis, c + dis));
         points.add(new PointF(c * (1 + 3 * g2) / 3f + dis, c * (3 - g2) / 3f + dis));
         points.add(new PointF(c * (1 + g2) / 3f + shapeWidth / g2 / 2 + dis, c * (3 + g2) / 3f + shapeWidth / g2 / 2 + dis));
-        points.add(new PointF(c * (1 + g2) / 3f - shapeWidth / g2 / 2 + dis, c * (3 + g2) / 3f + shapeWidth / g2 / 2 + dis));
+        points.add(new PointF(c * (1 + g2) / 3f - shapeWidth / g2 / 2 + dis, c * (3 + g2) / 3f + shapeWidth / g2 / 2 + dis));*/
     }
 
     //set background circle color
@@ -132,11 +153,17 @@ public class OpAnimationView extends View {
         paint.setStrokeWidth(width);
     }
 
+    //set the add_shape's radius
+    public void setShapeRadius(int radius) {
+        shapeRadius = radius;
+
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
 
         canvas.drawCircle(width / 2, height / 2, r, paintShadow);
-
+/*
         if (isRgihtShape) { // right --> add
             float x0 = (points.get(0).x - points.get(4).x) * progress + points.get(4).x;
             float y0 = (points.get(0).y - points.get(4).y) * progress + points.get(4).y;
@@ -159,6 +186,36 @@ public class OpAnimationView extends View {
 
             float x2 = points.get(2).x;
             float y2 = points.get(2).y;
+            float x3 = (points.get(5).x - points.get(3).x) * progress + points.get(3).x;
+            float y3 = (points.get(5).y - points.get(3).y) * progress + points.get(3).y;
+            canvas.drawLine(x0, y0, x1, y1, paint);
+            canvas.drawLine(x2, y2, x3, y3, paint);
+        }*/
+
+        if (isRgihtShape) { // right --> add
+            float x0 = (points.get(0).x - points.get(7).x) * progress + points.get(7).x;
+            float y0 = (points.get(0).y - points.get(7).y) * progress + points.get(7).y;
+            float x1 = (points.get(1).x - points.get(6).x) * progress + points.get(6).x;
+            float y1 = (points.get(1).y - points.get(6).y) * progress + points.get(6).y;
+
+            float x2 = (points.get(2).x - points.get(4).x) * progress + points.get(4).x;
+            float y2 = (points.get(2).y - points.get(4).y) * progress + points.get(4).y;
+
+            float x3 = (points.get(3).x - points.get(5).x) * progress + points.get(5).x;
+            float y3 = (points.get(3).y - points.get(5).y) * progress + points.get(5).y;
+            canvas.drawLine(x0, y0, x1, y1, paint);
+            canvas.drawLine(x2, y2, x3, y3, paint);
+
+        } else { //add --> right
+
+            float x0 = (points.get(7).x - points.get(0).x) * progress + points.get(0).x;
+            float y0 = (points.get(7).y - points.get(0).y) * progress + points.get(0).y;
+            float x1 = (points.get(6).x - points.get(1).x) * progress + points.get(1).x;
+            float y1 = (points.get(6).y - points.get(1).y) * progress + points.get(1).y;
+
+            float x2 = (points.get(4).x - points.get(2).x) * progress + points.get(2).x;
+            float y2 = (points.get(4).y - points.get(2).y) * progress + points.get(2).y;
+
             float x3 = (points.get(5).x - points.get(3).x) * progress + points.get(3).x;
             float y3 = (points.get(5).y - points.get(3).y) * progress + points.get(3).y;
             canvas.drawLine(x0, y0, x1, y1, paint);
